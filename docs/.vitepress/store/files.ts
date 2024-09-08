ee17769cc129c0189c3f72975abbc7be3efc9d5b
Kiwi2333 <1329634286@qq.com>
2024-09-08 10:29:17 +0800

    Update: 主页项目列表修改

--- docs/.vitepress/store/files.ts
+++ docs/.vitepress/store/files.ts
@@ -6,24 +6,25 @@ export const projectList: CardItem[] = [
   {
     title: "极物圈社区商城",
     icon: "/index/jiwuquan.png",
-    link: "https://jiwu.kiwi2333.top/",
+    link: "/项目/极物圈",
     details: "极物圈，是一个集商城、ai聊天、沟通、社区功能于一体的平台。"
   },
   {
     title: "JiwuChat 聊天应用",
     icon: "/index/jiwuchat-tauri.png",
     link: "/项目/极物聊天",
-    linkTarget: "_self",
     details: "Jiwu聊天应用，是一个ai聊天、社交功能于一体的Tauri桌面应用。"
   },
   {
     title: "MSI",
+    linkTarget: "_blank",
     icon: "https://www.apple.com/favicon.ico",
     link: "https://github.com/KiWi233333/MSI-GS66-Stealth-OC-Hackintosh-opencore/releases/tag/1.0.1",
     details: "微星GS66黑苹果EFI文件 OC-Hackintosh-opencore"
   },
   {
     title: "简约博客",
+    linkTarget: "_blank",
     icon: "/kiwi-fruit.svg",
     link: "https://blogs.kiwi233.top/",
     details: "H5C3+js+jq+bootstrap5响应式博客简约静态网站"
