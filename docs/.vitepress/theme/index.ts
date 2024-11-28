8de61ca07c8e1f079e1e36bca8cf1d1c02bb4a41
Kiwi2333 <1329634286@qq.com>
2024-11-29 01:56:12 +0800

    Update: 删除firebase

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -8,7 +8,6 @@ import './style/custom.scss'
 import NavBarTitle from './layout/NavBarTitle.vue'
 import PublicResource from '../compnents/PublicResource.vue'
 import 'uno.css'
-import { initializeApp } from "firebase/app";
 // import 'virtual:unocss-devtools'
 
 export default {
@@ -22,18 +21,6 @@ export default {
     //   }
     // }
     if (inBrowser) {
-      // 1. 引入 firebase.google 分析
-      // https://firebase.google.com/docs/web/setup#available-libraries
-      const firebaseConfig = {
-        apiKey: "AIzaSyA3lMOxV94oIbzyRNfc9_tHibQaeoffsrU",
-        authDomain: "jiwuquan.firebaseapp.com",
-        projectId: "jiwuquan",
-        storageBucket: "jiwuquan.firebasestorage.app",
-        messagingSenderId: "621147828247",
-        appId: "1:621147828247:web:a343702b7d2ce5cbc1ec52"
-      };
-      // Initialize Firebase
-      const app = initializeApp(firebaseConfig);
     }
     // 注册全局组件
     app.component("NavBarTitle", NavBarTitle)
